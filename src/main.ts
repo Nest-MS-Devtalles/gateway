b730c9669068b40146b5ac0d019a66db1aa9d187
valdo <osvaldo@lvvdigital.com>
2024-07-17 12:49:22 -0600

    feat: update console log message in main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log('Hola mundo - primer cambio !!')
+  console.log('Hola mundo - segundo cambio !!');
 
   logger.log(`Gateway is running on: http://localhost:${envs.port}`);
 }
