8eb9ada32cefa3be5b76b3128089503207e87a69
valdo <osvaldo@lvvdigital.com>
2024-07-30 17:15:37 -0600

    feat: add authentication module and controller

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { NatsModule } from '../transports/nats.module';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [NatsModule],
+  controllers: [AuthController],
+})
+export class AuthModule {}
